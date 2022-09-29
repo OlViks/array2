e7db933f1d2ec5c99b443ed06f7423637e1117ad
OlViks <salova.40@yandex.ru>
2022-09-29 17:23:54 +0300

    сделал второе задание

--- src/Array.java
+++ src/Array.java
@@ -1,5 +1,39 @@
 public class Array {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Массив 2.1");
+
+        int[] spent = generateRandomArray();
+
+        System.out.println("Задание №1");
+
+        int sum = 0;
+        for (int spen : spent){
+            sum += spen;
+        }
+        System.out.println("Сумма трат за месяц составила "+ sum +" рублей");
+
+        System.out.println("Задание №2");
+        int min = spent[0];
+        for (int j : spent) {
+            if (j < min)
+                min = j;
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min +" рублей.");
+        int max = spent[0];
+        for (int j : spent) {
+            if (j > max)
+                max = j;
+        }
+        System.out.println("Максимальная сумма трат за день составила " + max + " рублей.");
     }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+
+    }
+
 }
